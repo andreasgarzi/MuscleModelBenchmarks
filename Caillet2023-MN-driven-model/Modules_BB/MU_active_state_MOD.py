708d9e24193ee1fdd2170bf7557f4889cc0f59b1
Andrea Sgarzi <z5517249@ad.unsw.edu.au>
2025-04-07 19:21:59 +1000

    Updated active state to Hussein modified for ascending and descending phases in the tune_activestate script

--- Caillet2023-MN-driven-model/Modules_BB/MU_active_state_MOD.py
+++ Caillet2023-MN-driven-model/Modules_BB/MU_active_state_MOD.py
@@ -13,21 +13,15 @@ ODE that defines the dynamics of MU active state, from an input concentration of
 
 def MU_active_state_func(t, Ca, act):
 
-    #Ca = Ca/(5.5877*10**-6)
-    #Ca = Ca/(1.8*10**-5)
-    Ca = Ca/(9*10**-6)
-    
+    Ca = Ca*2e5
     amin = 1*10**-9
     ac = (act - amin)/(1 - amin)
     
-    n = 2.5  # species & exp conitions dependent parameter
+    k3, k4 = 15.5, 22.2  
     
     if Ca > ac:
-        K = 25.79  # 15.79
-        dadt = (Ca**n - ac)*(ac + K)*(1 - ac)
+        dadt = -(k4*ac - k3*Ca)*(1 - ac)
     else:
-        K = 60.8329 # 40.8329
-        dadt = (Ca - ac)*(ac + K) # decay following a non-normalized trend
+        dadt = -(k4*ac - k3*Ca)
     
-         
     return dadt
